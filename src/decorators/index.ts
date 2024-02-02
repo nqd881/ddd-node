d43aa7d8121a6de19467c8840e6fddf3e31be400
nqd881 <nqd881@gmail.com>
2024-02-02 16:41:33 +0700

    export decorators

---
+++ src/decorators/index.ts
@@ -0,0 +1,6 @@
+export * from "./aggregate";
+export * from "./command";
+export * from "./entity";
+export * from "./event";
+export * from "./model";
+export * from "./value-object";
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./core";
+export * from "./decorators";
 export * from "./types";
 export * from "./utils";
