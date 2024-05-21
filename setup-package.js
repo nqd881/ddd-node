31c0d3f3c947bbe50fcf572110a1ab6571facbfb
nqd881 <nqd881@gmail.com>
2024-05-21 11:22:54 +0700

    refactor setup-package.js

--- setup-package.js
+++ setup-package.js
@@ -1,18 +1,24 @@
 import fs from "fs";
-import path from "path";
 import packageJson from "./package.json" assert { type: "json" };
 
-const buildDir = "./dist";
-
-packageJson.type = "commonjs";
-packageJson.main = "index.js";
-packageJson.types = "index.d.ts";
-
 const createCjsPackageJson = () => {
+  const packageJsonFileContent = () => {
+    const result = packageJson;
+
+    result.type = "commonjs";
+    result.main = "index.js";
+    result.types = "index.d.ts";
+
+    delete result.scripts;
+    delete result.mocha;
+
+    return JSON.stringify(result);
+  };
+
   const packageJsonFilePath = "./dist/package.json";
 
   if (!fs.existsSync(packageJsonFilePath)) {
-    fs.writeFile(packageJsonFilePath, JSON.stringify(packageJson), (error) => {
+    fs.writeFile(packageJsonFilePath, packageJsonFileContent(), (error) => {
       if (error) throw error;
     });
   }
